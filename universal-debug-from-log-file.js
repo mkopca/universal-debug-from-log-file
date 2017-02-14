83030803939bb1bddc04a4ae28e3672f98a2f39a
mkopca <milan.kopca.sunt@gmail.com>
2017-02-14 18:06:41 +0100

    init

---
+++ universal-debug-from-log-file.js
@@ -0,0 +1,23 @@
+/**
+ * Addon read log file placed on the website server. Log file is a plain text, or can be a json formatted array of
+ * many logs of different types.
+ * Addon read and parse this log file. Then show humanized results.
+ * Possible structure of json, if we want show some categorized results or if we want filter history records:
+ * @todo
+ *
+ * Another functions of addon:
+ * @todo
+ *
+ * @summary Extension read log file from the website server, parse it and show humanized results
+ * author   mkopca
+ * version  1.0
+ * since    2017-02-14
+ */
+
+function readLogFile() {
+    console.log('test');
+}
+
+jQuery(document).ready(function(){
+    readLogFile();
+});
